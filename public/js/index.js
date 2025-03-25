26e2f004c212279e420108710300349830917aec
Jeff Lang <jeff.m.lang@gmail.com>
2025-03-24 20:50:31 -0700

    Started on the custom email validation using regex

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,27 @@ const emailForm = document.getElementById('email-capture');
 const emailInput = document.getElementById('email-input');
 const errorMsg = document.getElementById('error-message');
 
+const prefixPattern = /^(?=.{1,64}$)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*$/;
+
 emailForm.addEventListener('submit', (e) => {
-  emailInput.classList.add('error');
-  errorMsg.classList.add('error');
+
+  e.preventDefault();
+  const prefix = emailInput.value;
+
+  // console.log(prefixPattern.test(prefix));
+
+  const atPos = findAt(prefix);
+  if(atPos < 1 || atPos > prefix.length - 2) { 
+    console.log('error');
+    return;
+  };
+
+  console.log(findAt(prefix));
+
+  // emailInput.classList.add('error');
+  // errorMsg.classList.add('error');
 });
+
+function findAt(str) {
+  return str.indexOf('@');
+}
