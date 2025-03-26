e93e250211ca02c13a13c61f98c1596cdbe5d9e6
Jeff Lang <jeff.m.lang@gmail.com>
2025-03-25 20:28:50 -0700

    Added fetch to get top level domains from iana

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,25 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+async function getTLDlist() {
+
+  const url = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt';
+  try {
+    const response = await fetch(url);
+    if(!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const file = await response.text();
+    if(!file) {
+      throw new Error(`Error with file stream`);
+    }
+
+  }
+  catch (error) {
+    console.error(error.message);
+  }
+}
+
 // Basic start of custom email validation
 const emailForm = document.getElementById('email-capture');
 const emailInput = document.getElementById('email-input');
@@ -20,22 +39,32 @@ const prefixPattern = /^(?=.{1,64}$)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0
 emailForm.addEventListener('submit', (e) => {
 
   e.preventDefault();
-  const prefix = emailInput.value;
+  const userEmail = emailInput.value;
+
+  try {
+    const emailArray = userEmail.split('@');
+    if(emailArray.length !== 2) throw new Error('Missing @ symbol');
+
+    const prefix = emailArray[0];
+    if (!prefixPattern.test(prefix)) throw new Error('Invalid email prefix');
+
+    const domains = emailArray[1].split('.');
+    console.log(domains);
+
+    emailInput.classList.remove('error');
+    errorMsg.classList.remove('error');
+  }
+  catch (error) {
+    console.error(`Error: ${error.message}`);
+    emailInput.classList.add('error');
+    errorMsg.classList.add('error');
+  }
+
+
 
   // console.log(prefixPattern.test(prefix));
 
-  const atPos = findAt(prefix);
-  if(atPos < 1 || atPos > prefix.length - 2) { 
-    console.log('error');
-    return;
-  };
-
-  console.log(findAt(prefix));
 
   // emailInput.classList.add('error');
   // errorMsg.classList.add('error');
 });
-
-function findAt(str) {
-  return str.indexOf('@');
-}
