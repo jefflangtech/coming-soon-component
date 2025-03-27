c44c1b3dd7ec5b653df2e4f5241cc65ddbbac66c
Jeff Lang <jeff.m.lang@gmail.com>
2025-03-26 22:19:37 -0700

    Finished basic functionality for robust email validation

--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,28 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+// Fetch the top level domains list - text file
 async function getTLDlist() {
 
-  const url = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt';
+  const cacheKey = 'tldList';
+  const cacheTimeKey = 'tldTimeStamp';
+  const maxAge = 1000 * 60 * 60 * 24 * 7;
+
+  console.log('Checking cache for TLD file');
+  const cachedData = localStorage.getItem(cacheKey);
+  const cachedTime = localStorage.getItem(cacheTimeKey);
+
+  const isFresh = cachedData && cachedTime && (Date.now() - Number(cachedTime) < maxAge);
+
+  if(isFresh) {
+    console.log('Using fresh cached TLD data');
+    return cachedData;
+  }
+
+
   try {
+    console.log('Fetching TLD data file');
+    const url = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt';
     const response = await fetch(url);
     if(!response.ok) {
       throw new Error(`Response status: ${response.status}`);
@@ -23,10 +41,29 @@ async function getTLDlist() {
       throw new Error(`Error with file stream`);
     }
 
+    localStorage.setItem(cacheKey, file);
+    localStorage.setItem(cacheTimeKey, Date.now().toString());
+
+    return file;
   }
   catch (error) {
     console.error(error.message);
+    if(cachedData) {
+      console.log('Using old TLD cache');
+      return cachedData;
+    }
   }
+}
+
+async function parseTLDFile() {
+  const tldFile = await getTLDlist();
+  console.log('TLD list loaded');
+
+  const parsedTLDs = tldFile.split('\n').filter(line => line && !line.startsWith('#')).map(tld => tld.toLowerCase());
+
+  console.log(`Array of TLDs length: ${parsedTLDs.length}`);
+
+  return parsedTLDs;
 }
 
 // Basic start of custom email validation
@@ -35,13 +72,16 @@ const emailInput = document.getElementById('email-input');
 const errorMsg = document.getElementById('error-message');
 
 const prefixPattern = /^(?=.{1,64}$)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*$/;
+const domainPattern = /^(?=.{1,63}$)[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/;
 
-emailForm.addEventListener('submit', (e) => {
+emailForm.addEventListener('submit', async (e) => {
 
   e.preventDefault();
-  const userEmail = emailInput.value;
+  const userEmail = emailInput.value.toLowerCase();
+  const tldList = await parseTLDFile();
 
   try {
+
     const emailArray = userEmail.split('@');
     if(emailArray.length !== 2) throw new Error('Missing @ symbol');
 
@@ -49,10 +89,22 @@ emailForm.addEventListener('submit', (e) => {
     if (!prefixPattern.test(prefix)) throw new Error('Invalid email prefix');
 
     const domains = emailArray[1].split('.');
-    console.log(domains);
+    if(domains.length <= 1) throw new Error('Improper domain format');
+
+    const tld = domains.pop();
+    if(!tldList.includes(tld)) throw new Error('Invalid TLD');
+
+    for(const domain of domains) {
+      if(!domainPattern.test(domain)) throw new Error('Invalid DNS label');
+    }
+
+    console.log(`Email: ${userEmail}, prefix: ${prefix}, hostname: ${emailArray[1]}`);
 
     emailInput.classList.remove('error');
     errorMsg.classList.remove('error');
+
+    emailInput.value = '';
+
   }
   catch (error) {
     console.error(`Error: ${error.message}`);
